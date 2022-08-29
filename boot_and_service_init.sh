3ebd10aa81fad143e733aef4355b8ac5b666feaa
arthurtwks <lion811004@gmail.com>
2022-08-29 19:41:53 +0800

    add script description

--- boot_and_service_init.sh
+++ boot_and_service_init.sh
@@ -28,4 +28,9 @@ fi
 docker-compose up --build --detach
 
 docker-compose exec apache-server /bin/sh -c "cd /usr/local/apache2/bin/ && ./apachectl -k start"
+
+# TODO - bugfix: why using docker-compose exec causing zombie java process
+
+# Now need to enter container and then start start.sh manually
 # docker-compose exec tomee1-server /bin/sh -c "/home/user/temp/apache-tomee-plus-7.1.4/bin/startup.sh"
+# docker-compose exec tomee2-server /bin/sh -c "/home/user/temp/apache-tomee-plus-7.1.4/bin/startup.sh"
